c58711865cee9d91686cfc44ecb438f222613b38
Alan <alanbustox46@gmail.com>
2023-08-17 18:15:09 -0400

    prueba subida a prod

--- next.config.js
+++ next.config.js
@@ -3,4 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
+// next.config.js
+module.exports = {
+  serverRuntimeConfig: {
+    // Configuraciones de tiempo de ejecución del servidor
+  },
+  publicRuntimeConfig: {
+    port: parseInt(process.env.PORT, 10) || 3000, // Puerto predeterminado: 3000
+  },
+};
+
 module.exports = nextConfig;
